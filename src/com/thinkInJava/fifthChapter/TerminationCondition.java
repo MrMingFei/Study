889cb65837f252b91aae0ebb8b2301fa7f88efae
MrMingFei <18330368050@163.com>
2018-07-04 22:51:50 +0800

    add

---
+++ src/com/thinkInJava/fifthChapter/TerminationCondition.java
@@ -0,0 +1,32 @@
+package com.thinkInJava.fifthChapter;
+
+//: initialization/TerminationCondition.java
+//Using finalize() to detect an object that
+//hasn't been properly cleaned up.
+class Book{
+    boolean checkedOut = false;
+    Book(boolean checkOut){
+        checkedOut = checkOut;
+    }
+    void checkIn(){
+        checkedOut = false;
+    }
+    protected void finalize(){
+        if (checkedOut){
+            System.out.println("Error: check out");
+            //Normally, you`ll also do this:
+            //super.finalize();//Call the base-class version
+        }
+    }
+}
+public class TerminationCondition {
+    public static void main(String[] args){
+        Book novel = new Book(true);
+        //Proper cleanup:
+        novel.checkIn();
+        //Drop the reference, forget to clean up:
+        new Book(true);
+        //Force garbage collection & finalization:
+        System.gc();
+    }
+}
