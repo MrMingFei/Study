4becb27ac4eaa41cf1bb00f7f4196c9e63b5e1e0
MrMingFei <18330368050@163.com>
2018-07-27 17:27:27 +0800

    2018/7/27 at company

---
+++ src/com/thinkInJava/tenthchapter/controller/Controller.java
@@ -0,0 +1,22 @@
+package com.thinkInJava.tenthchapter.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private List<Event> eventList = new ArrayList<Event>();
+    public void addEvent(Event event){
+        eventList.add(event);
+    }
+    public void run(){
+        while (eventList.size() > 0){
+            for (Event e : new ArrayList<Event>(eventList)){
+                if (e.ready()){
+                    System.out.println(e);
+                    e.action();
+                    eventList.remove(e);
+                }
+            }
+        }
+    }
+}
